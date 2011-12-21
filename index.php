517fcfaf7292f224f61379271d32f5cbecd2159c
Owen Mundy <omundy@gmail.com>
2011-12-21 17:00:00 -0500

    Updated title(s)

--- index.php
+++ index.php
@@ -3,20 +3,20 @@
 /* 
  * 	Copyright 2011 Owen Mundy 
  *
- *	This file is part of Freedom for Our Files (Facebook Canvas App Starter page).
+ *	This file is part of Facebook Canvas Starter App.
  *
- *	Freedom for Our Files is free software: you can redistribute it and/or modify
+ *	Facebook Canvas Starter App is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
  *	the Free Software Foundation, either version 3 of the License, or
  *	(at your option) any later version.
  *	
- *	Freedom for Our Files is distributed in the hope that it will be useful,
+ *	Facebook Canvas Starter App is distributed in the hope that it will be useful,
  *	but WITHOUT ANY WARRANTY; without even the implied warranty of
  *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *	GNU General Public License for more details.
  *	
  *	You should have received a copy of the GNU General Public License
- *	along with Freedom for Our Files.  If not, see <http://www.gnu.org/licenses/>.
+ *	along with Facebook Canvas Starter App.  If not, see <http://www.gnu.org/licenses/>.
  */ 
   
 
